a69ce9f3a486d7a5e885a39e5f2727d566dd1403
Claire Chabas <claire.chabas@gmail.com>
2023-01-19 19:02:48 +0000

    feat(algorithms): add recursive solution with helper function to capitalizeWords

--- algorithms/upperCasedWords.js
+++ algorithms/upperCasedWords.js
@@ -1,5 +1,7 @@
 // Given an array of words, return a new array containing
 // each word capitalized, meaning uppercased.
+
+// Option 1: pure recursion
 function capitalizeWords(words) {
   let result = [];
 
@@ -9,6 +11,20 @@ function capitalizeWords(words) {
     return result.concat(capitalizeWords(words.splice(1)));
   }
 
+  return result;
+}
+
+// Option 2: with recusive helper function
+function capitalizeWords(arr) {
+  let result = [];
+
+  function helper(inputArr) {
+    while (inputArr.length > 0) {
+      result.push(inputArr[0].toUpperCase());
+      return helper(inputArr.slice(1));
+    }
+  }
+  helper(arr);
   return result;
 }
 
