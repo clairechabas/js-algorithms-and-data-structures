0e773e30ab93a6292ac37888369e461ba912a06e
Claire Chabas <claire.chabas@gmail.com>
2023-01-22 12:42:27 +0000

    docs: commenting bubble sort implem

--- sortingAlgorithms/bubbleSort.js
+++ sortingAlgorithms/bubbleSort.js
@@ -1,3 +1,7 @@
+// Bubble sort compares each value of an array with the next
+// and swaps them to move the higher values up to the end of the array.
+// At each loop you compare a smaller version of the array.
+// Time Complexity: O(n^2)
 // ES5
 function bubbleSort(arr) {
   for (let i = arr.length; i > 0; i--) {
@@ -31,6 +35,10 @@ function bubbleSort(arr) {
 }
 
 // ES6 Optimized version for a nearly sorted array
+// We use an indicator to break the loop when we know we don't need to
+// swap. This avoids us to check each value of the array again while we
+// already did in the previous loop and saw that there was no need to swap.
+// Time Complexity: O(n)
 function bubbleSort(arr) {
   const swap = (arr, idx1, idx2) =>
     ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
