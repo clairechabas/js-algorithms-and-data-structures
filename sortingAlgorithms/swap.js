ffce38a4d555cc87b040c4d631797590c90b1bb9
Claire Chabas <claire.chabas@gmail.com>
2023-01-22 12:01:05 +0000

    feat(searching-algorithms): defining a swap helper

---
+++ sortingAlgorithms/swap.js
@@ -0,0 +1,11 @@
+// ES5
+function swap(arr, index1, index2) {
+  var temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
+}
+
+// ES6
+function swap(arr, index1, index2) {
+  [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+}
