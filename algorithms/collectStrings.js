2d0953073c9b3e69cbe3f61f4d8ee167cd2bfc5e
Claire Chabas <claire.chabas@gmail.com>
2023-05-04 15:39:46 +0200

    feat(algos): recursion solutions

--- algorithms/collectStrings.js
+++ algorithms/collectStrings.js
@@ -1,30 +1,13 @@
-// Write a function which accepts an object and returns an array
-// of all the values in the object that have a typeof string.
-function collectStrings(obj) {
-  let result = [];
+/**
+ * collectStrings
+ *
+ * Write a function which accepts an object and returns an array
+ * of all the values in the object that have a typeof string.
+ * @param {*} obj
+ * @returns array
+ */
 
-  function helper(object) {
-    while (Object.keys(object).length > 0) {
-      for (const key in object) {
-        if (typeof object[key] !== "object") {
-          if (typeof object[key] === "string") {
-            result.push(object[key]);
-          }
-        } else {
-          helper(object[key]);
-        }
-
-        delete object[key];
-      }
-    }
-
-    return result;
-  }
-
-  helper(obj);
-
-  return result;
-}
+function collectStrings(obj) {}
 
 const obj = {
   stuff: "foo",
@@ -43,3 +26,30 @@ const obj = {
 };
 
 console.log(collectStrings(obj)); // ["foo", "bar", "baz"])
+
+// Previous solution
+// function collectStrings(obj) {
+//   let result = [];
+
+//   function helper(object) {
+//     while (Object.keys(object).length > 0) {
+//       for (const key in object) {
+//         if (typeof object[key] !== "object") {
+//           if (typeof object[key] === "string") {
+//             result.push(object[key]);
+//           }
+//         } else {
+//           helper(object[key]);
+//         }
+
+//         delete object[key];
+//       }
+//     }
+
+//     return result;
+//   }
+
+//   helper(obj);
+
+//   return result;
+// }
--- algorithms/stringifyNumbers.js
+++ algorithms/stringifyNumbers.js
@@ -1,51 +1,54 @@
-// Write a function which takes an object and finds all of the values
-// which are numbers and converts them to strings.
+/**
+ * stringifyNumbers
+ *
+ * Write a function which takes an object and finds all of
+ * the values which are numbers and converts them to strings.
+ *
+ * @param {object} obj
+ * @returns object
+ */
 
-// Option 1: with recursive helper function
+// Solution 1: modifying the original object
 function stringifyNumbers(obj) {
-  let newObj = {};
+  for (const key in obj) {
+    // if it's a number convert to string
+    if (typeof obj[key] === "number") {
+      obj[key] = obj[key].toString();
+    } else if (obj[key] && typeof obj[key] === "object") {
+      // if it's an array or an object loop inside and repeat
+      stringifyNumbers(obj[key]);
+    }
+  }
+
+  return obj;
+}
+
+// Solution 2: creating a new object
+function stringifyNumbers2(obj) {
+  let result = {};
 
   function helper(object) {
     for (const key in object) {
-      if (typeof object[key] !== "object") {
-        if (Number.isFinite(object[key])) {
-          newObj[key] = object[key].toString();
-        } else {
-          newObj[key] = object[key];
-        }
-      } else if (Array.isArray(object[key]) || object[key] === null) {
-        newObj[key] = object[key];
+      // if it's a number convert to string
+      if (typeof object[key] === "number") {
+        result[key] = object[key].toString();
+      } else if (
+        object[key] &&
+        typeof object[key] === "object" &&
+        !Array.isArray(object[key])
+      ) {
+        // if it's an array or an object loop inside and repeat
+        result[key] = stringifyNumbers(object[key]);
       } else {
-        newObj[key] = stringifyNumbers(object[key]);
+        result[key] = object[key];
       }
     }
-
-    return newObj;
   }
-
   helper(obj);
 
-  return newObj;
+  return result;
 }
 
-// Option 2: pure recursion
-function stringifyNumbers(obj) {
-  let newObj = {};
-
-  for (let key in obj) {
-    if (typeof obj[key] === "number") {
-      newObj[key] = obj[key].toString();
-    } else if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
-      newObj[key] = stringifyNumbers(obj[key]);
-    } else {
-      newObj[key] = obj[key];
-    }
-  }
-
-  return newObj;
-}
-
-// Example
 let obj = {
   num: 1,
   test: [],
@@ -57,4 +60,63 @@ let obj = {
     },
   },
 };
-console.log(stringifyNumbers(obj));
+console.log(stringifyNumbers2(obj));
+
+// Previous solutions
+// Option 1: with recursive helper function
+// function stringifyNumbers(obj) {
+//   let newObj = {};
+
+//   function helper(object) {
+//     for (const key in object) {
+//       if (typeof object[key] !== "object") {
+//         if (Number.isFinite(object[key])) {
+//           newObj[key] = object[key].toString();
+//         } else {
+//           newObj[key] = object[key];
+//         }
+//       } else if (Array.isArray(object[key]) || object[key] === null) {
+//         newObj[key] = object[key];
+//       } else {
+//         newObj[key] = stringifyNumbers(object[key]);
+//       }
+//     }
+
+//     return newObj;
+//   }
+
+//   helper(obj);
+
+//   return newObj;
+// }
+
+// // Option 2: pure recursion
+// function stringifyNumbers(obj) {
+//   let newObj = {};
+
+//   for (let key in obj) {
+//     if (typeof obj[key] === "number") {
+//       newObj[key] = obj[key].toString();
+//     } else if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+//       newObj[key] = stringifyNumbers(obj[key]);
+//     } else {
+//       newObj[key] = obj[key];
+//     }
+//   }
+
+//   return newObj;
+// }
+
+// // Example
+// let obj = {
+//   num: 1,
+//   test: [],
+//   data: {
+//     val: 4,
+//     info: {
+//       isRight: true,
+//       random: 66,
+//     },
+//   },
+// };
+// console.log(stringifyNumbers(obj));
