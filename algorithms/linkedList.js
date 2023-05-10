377af1b28bd77e3be6cb34fcdad803cdba9c8e8e
Claire Chabas <claire.chabas@gmail.com>
2023-05-10 11:38:04 +0200

    feat(algos): linked list max twin sum

--- algorithms/linkedList.js
+++ algorithms/linkedList.js
@@ -180,3 +180,58 @@ const swapNodesInPairs = function (head) {
 
   return newStart;
 };
+
+/**
+ * Ex 7: Maximum Twin Sum of a Linked List
+ *
+ * In a linked list of size n, where n is even, the ith node (0-indexed) of the linked
+ * list is known as the twin of the (n-1-i)th node, if 0 <= i <= (n / 2) - 1.
+ *
+ * For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the twin of node 2.
+ * These are the only nodes with twins for n = 4
+ * The twin sum is defined as the sum of a node and its twin.
+ *
+ * Given the head of a linked list with even length, return the maximum twin sum of the linked list.
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+// Solution with slow/fast pointers and list reversal - 124ms - Beats 94.12%
+var maxTwinSum = function (head) {
+  // 1. find the middle of the list  using fast & slow pointers
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  // now slow is located at the middle of the list
+
+  // 2. reverse the second half of the list to have each twin node located at n/2
+  let middle = slow;
+  let prev = null;
+  while (middle) {
+    let nextNode = middle.next;
+
+    middle.next = prev;
+    prev = middle;
+
+    middle = nextNode;
+  }
+  // now prev is located at the middle of the list
+
+  // 3. use fast & slow pointers again, fast starting at n/2, and update the sum at each iteration with the max
+  let start = head;
+  let twin = prev;
+  let maxSum = 0;
+
+  while (start && twin) {
+    maxSum = Math.max(maxSum, start.val + twin.val);
+
+    start = start.next;
+    twin = twin.next;
+  }
+
+  return maxSum;
+};
